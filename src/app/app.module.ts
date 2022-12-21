78a4574844e794ab4115275e003caecf547987db
vanderlivrlj <vanderlidev@gmail.com>
2022-12-21 12:56:49 -0300

    Property Binding - Interpolação Class e Style binding Primeira e segunda aula sobre Data Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CursosModule} from "./cursos/cursos.module";
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBindingComponent
   ],
   imports: [
     BrowserModule,

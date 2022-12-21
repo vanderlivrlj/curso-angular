452c40dd2b0dd758f879096b4fe4a26a78c78ab9
vanderlivrlj <vanderlidev@gmail.com>
2022-12-21 09:30:42 -0300

    módulo de introdução do curso de angular

---
+++ src/app/cursos/cursos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { CursosService } from "./cursos.service";
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrls: ['./cursos.component.scss']
+})
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+  cursos: string [];
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://www.google.com'
+
+    // Intanciando uma classe nova. Maneira antiga
+   // var servico = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
+  }
+
+  ngOnInit(): void {
+  }
+
+}

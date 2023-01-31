937a5a66794df32df9d858b8d91259cb1442f279
vanderlivrlj <vanderlidev@gmail.com>
2023-01-31 15:07:22 -0300

    Revert "Iniciando módulo de pipe"
    This reverts commit 408608414bb3883d399189593cbe88620a958b1a.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ExemplosPipesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

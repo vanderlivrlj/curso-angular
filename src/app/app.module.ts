4737c663b9a2cbce444c82e36ba899be7acac5a9
vanderlivrlj <vanderlidev@gmail.com>
2023-03-19 09:41:41 -0300

    Ultima versão curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import {FormsModule} from "@angular/forms";
+import {TemplateFormModule} from "./template-form/template-form.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    TemplateFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
